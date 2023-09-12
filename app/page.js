c98a85341d683223d1246ee7fa4dfe0194e0f758
CodingClardy <28507502+CodingClardy@users.noreply.github.com>
2023-09-12 13:40:24 -0700

    adding styles

--- app/page.js
+++ app/page.js
@@ -1,29 +1,17 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
-function Bs(props) {
-  return (
-    <div className={`${styles.card} ${styles.center}`}>
-      <ul>
-        <li>{props.element} -Sushi Roll</li>
-      </ul>
-    </div>
-  );
-}
-
 export default async function Home() {
   const res = await fetch('https://catfact.ninja/fact');
   const str = await res.json();
-  const arry = ['Skywalker Roll', 'Rock & Roll', 'Puyallup Roll'];
   return (
     <>
-      <main className={styles.main}>{str.fact}</main>
-      <div>
-        <h2 className={styles.center}>My Sushi Order</h2>
-        {arry.map((el) => (
-          <Bs element={el} word="banana" />
-        ))}
-      </div>
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h2>Anonymous</h2>
+          <div>{str.fact}</div>
+        </div>
+      </main>
     </>
   );
 }
