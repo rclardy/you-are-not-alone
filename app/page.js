6f44ac597de7d3773aeb1c57288802acb60546c2
CodingClardy <28507502+CodingClardy@users.noreply.github.com>
2023-09-12 15:34:52 -0700

    added ApiCall component

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
-export default async function Home() {
+async function ApiCall() {
   const res = await fetch('https://catfact.ninja/fact');
   const str = await res.json();
+  const data = str.fact;
+  return data;
+}
+
+export default async function Home() {
+  const quote = ApiCall();
   return (
     <>
       <main className={styles.main}>
         <div className={styles.card}>
           <h2>Anonymous</h2>
-          <div>{str.fact}</div>
+          <div className={styles.cardText}>{quote}</div>
         </div>
       </main>
     </>
