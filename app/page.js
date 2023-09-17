938187caecc2dee0b5646f7ece97939710d13fdc
CodingClardy <28507502+CodingClardy@users.noreply.github.com>
2023-09-16 23:21:35 -0700

    trying to display the fetch info...

--- app/page.js
+++ app/page.js
@@ -1,33 +1,49 @@
 'use client';
 import Image from 'next/image';
 import styles from './page.module.css';
+import { useState, useEffect } from 'react';
 
-async function ApiCall() {
-  const res = await fetch('https://catfact.ninja/fact');
-  const str = await res.json();
-  const data = str.fact;
-  return data;
+function ApiCall() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>no facts</p>;
+
+  return <QuoteBox fact={data.fact} />;
 }
-function QuoteBox() {
-  const quote = ApiCall();
+
+function QuoteBox({ fact }) {
   return (
     <div className={styles.card}>
       <h2>Anonymous</h2>
-      <div className={styles.cardText}>{quote}</div>
+      <div className={styles.cardText}>{fact}</div>
       <Button />
     </div>
   );
 }
 
-const Button = ({ text, onClick }) => {
+const Button = ({ onClick }) => {
+  const handleClick = () => {
+    const newQoute = ApiCall();
+  };
   return (
-    <button type="button" onClick={ApiCall}>
+    <button type="button" onClick={handleClick}>
       Anotha One
     </button>
   );
 };
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <main className={styles.main}>
